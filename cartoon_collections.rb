e312670301218f6bf5145baf46111bec03401ce5
Irene Scott <irenejinscott@gmail.com>
2020-05-11 00:54:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,21 @@ end
 
 def find_the_cheese(strings)
   strings.detect do |cheese| 
-    strings.include?(cheese)
+    cheese_types.include?(cheese)
 end
 end
 
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
+end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
+find_the_cheese(snacks)
